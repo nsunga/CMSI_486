f8b666427278d71e0998077c35abb7e7bd3532b8
Rony Aguilar <raguil12@lion.lmu.edu>
2017-11-27 11:29:50 -0800

    Created Node and Relationships

--- app.py
+++ app.py
@@ -2,16 +2,42 @@ from py2neo import Graph, Node, Relationship
 import csv
 graph = Graph()
 
+with open('cases.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        case = Node("Case",
+                    case_id=row['case_id'],
+                    date=row['date'],
+                    crime_location=row['crime_location'])
+        graph.create(case)
+
+with open('crime.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        crime = Node("Crime",
+                    crime_id=row['crime_id'],
+                    crime_type=row['crime_type'])
+        graph.create(crime)
+        case = graph.find_one('Case','case_id',row['case_id'])
+        rel = Relationship(crime, "HAS", case)
+        graph.create(rel)
+
 with open('persons.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        node = Node("Person",
+        person = Node("Person",
                     first_name=row['first_name'],
                     last_name=row['last_name'],
                     age=row['age'],
                     sex=row['sex'],
                     ethnicity=row['ethnicity'])
-        if(row['suspect'] == 'Y')
+        graph.create(person)
+
+        if row['suspect'] == 'Y':
             crime = graph.find_one('Crime', 'crime_id', row['crime_id'])
-            rel = Relationship(node, "COMMITTED_A", crime)
-        graph.create(node)
+            rel = Relationship(person, "COMMITTED_A", crime)
+            graph.create(rel)
+
+        case = graph.find_one('Case','case_id', row['case_id'])
+        rel = Relationship(person, "INVOLVED_IN", case)
+        graph.create(rel)
